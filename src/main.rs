02c86e887defd75c6490161b5ae1e15b4a281ebf
Cassino <renatocassino@gmail.com>
2023-04-08 20:46:44 -0300

    feat: add opencv

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::{fs::File, io::{BufReader, Read}};
+use image::GenericImageView;
+use opencv::{core::{Mat, Rect}, prelude::MatTrait};
+use opencv::core::Vector;
 
-use actix_web::{get, web, App, HttpServer, Responder, http::{StatusCode, header}, HttpResponse};
+use actix_web::{get, web::{self, Query}, App, HttpServer, Responder, http::{StatusCode, header}, HttpResponse, HttpRequest};
+use serde::Deserialize;
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> HttpResponse {
@@ -9,17 +13,62 @@ async fn greet(name: web::Path<String>) -> HttpResponse {
         .body(format!("Hello {}!", name))
 }
 
+#[derive(Deserialize)]
+struct QueryParams {
+    w: i32,
+    h: i32,
+}
+
 #[get("/file/{name}")]
-async fn file(name: web::Path<String>) -> HttpResponse {
+async fn file(name: web::Path<String>, params: web::Query<QueryParams>) -> HttpResponse {
     let path = "./src/images/sun.jpg";
     let file = File::open(path).unwrap();
+
     let mut buf_reader = BufReader::new(file);
     let mut contents = Vec::new();
     buf_reader.read_to_end(&mut contents).unwrap();
 
+    // crop image to 300 x 200
+    let mut img = image::load_from_memory(&contents).unwrap();
+    let (width, height) = img.dimensions();
+    
+    println!("width: {}, height: {}", width, height);
+    println!("{:?}", img.color());
+
+    let cropped_image = img.crop(0, 0, params.w.try_into().unwrap(), params.h.try_into().unwrap());
+    cropped_image.save("sun-cropped.jpg").unwrap();
+
+    let file2 = File::open("sun-cropped.jpg").unwrap();
+    let mut buf_reader2 = BufReader::new(file2);
+    let mut contents_crop = Vec::new();
+    buf_reader2.read_to_end(&mut contents_crop).unwrap();
+
     HttpResponse::Ok()
         .append_header(("Content-Type", "image/jpeg"))
-        .body(contents)
+        .body(contents_crop)
+}
+
+#[get("/file-cv/{name}")]
+async fn file_cv(name: web::Path<String>, params: web::Query<QueryParams>) -> HttpResponse {
+    let path = "./src/images/sun.jpg";
+    let mut img = opencv::imgcodecs::imread(
+        path,
+        opencv::imgcodecs::IMREAD_COLOR,
+    ).unwrap();
+
+    let cropped_image = Mat::roi(&img, Rect {
+        x: 0,
+        y: 0,
+        width: params.w,
+        height: params.h,
+    }).unwrap();
+
+    let mut out_vector: Vector<u8> = Vector::new();
+    opencv::imgcodecs::imencode(".jpg", &cropped_image, &mut out_vector, &Vector::new()).expect("Encode image");
+
+    HttpResponse::Ok()
+        .append_header(("Content-Type", "image/jpeg"))
+        .body(out_vector.to_vec())
 }
 
 #[actix_web::main] // or #[tokio::main]
@@ -27,7 +76,7 @@ async fn main() -> std::io::Result<()> {
     let n_workers = num_cpus::get() * 2;
     println!("Starting server with {} workers", n_workers);
     HttpServer::new(|| {
-        App::new().service(greet).service(file)
+        App::new().service(greet).service(file).service(file_cv)
     })
     .workers(n_workers)
     .bind(("127.0.0.1", 8080))?
