13da5825984eeb7f73ddc24aa9999f119159bd1f
Cassino <renatocassino@gmail.com>
2023-04-09 17:03:52 -0300

    feat: add anyhow and thiserror to error handling

--- src/security.rs
+++ src/security.rs
@@ -1,19 +1,27 @@
 use hmac::{Hmac, Mac};
 use sha1::{Sha1};
 use base64::{Engine as _, engine::general_purpose};
+use thiserror::Error;
+use anyhow::Result;
 
 type HmacSha1 = Hmac<Sha1>;
 
-pub fn get_key_by_path(path: String) -> String {
-    let mut mac = HmacSha1::new_from_slice(b"MY_KEY").expect("HMAC can take key of any size");
+#[derive(Error, Debug)]
+pub enum KeyError {
+    #[error("Invalid HMAC key length")]
+    InvalidKeyLength(#[from] sha1::digest::InvalidLength),
+}
+
+pub fn get_key_by_path(path: String) -> Result<String, KeyError> {
+    let mut mac = HmacSha1::new_from_slice(b"MY_KEY")?;
     mac.update(path.as_bytes());
 
     let hmac_base64 = general_purpose::STANDARD.encode(mac.finalize().into_bytes());
-    hmac_base64.replace('+', "-").replace('/', "_")
+    Ok(hmac_base64.replace('+', "-").replace('/', "_"))
 }
 
 pub fn is_valid_key(key: String, path: String) -> bool {
-    get_key_by_path(path) == key
+    get_key_by_path(path).unwrap() == key
 }
 
 #[cfg(test)]
@@ -24,13 +32,13 @@ mod tests {
     #[test]
     fn test_get_key_by_path() {
         let mut path = "50x50/big.jpg".to_string();
-        assert_eq!(get_key_by_path(path), "sMxTvxyS2uudMVBgjPv_YfTFe3E=");
+        assert_eq!(get_key_by_path(path).unwrap(), "sMxTvxyS2uudMVBgjPv_YfTFe3E=");
 
         path = "300x200/http://picsum.photo/500/500.jpg".to_string();
-        assert_eq!(get_key_by_path(path), "KScb5yXHfcyeQd4evRzy4xiQoaE=");
+        assert_eq!(get_key_by_path(path).unwrap(), "KScb5yXHfcyeQd4evRzy4xiQoaE=");
 
         path = "300x200/smart/http://picsum.photo/500/500.jpg".to_string();
-        assert_eq!(get_key_by_path(path), "FJd9jVRAhh4rucHcwAlqAJyHyd8=");
+        assert_eq!(get_key_by_path(path).unwrap(), "FJd9jVRAhh4rucHcwAlqAJyHyd8=");
     }
 
     #[test]
